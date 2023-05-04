c885cc4e0b09516f0a31484837fd922f7ac6b7d0
CallumR2 <101304812+CallumR2@users.noreply.github.com>
2023-05-04 15:38:44 +1000

    Add files via upload
    Updates with temp red light functionality

--- motor_driver.py
+++ motor_driver.py
@@ -1,5 +1,7 @@
 import picamera
 import picamera.array
+import sys
+import numpy as np
 from time import sleep
 import RPi.GPIO as GPIO
 from classes import Vehicle, Channel
@@ -7,18 +9,46 @@ from classes import Vehicle, Channel
 #Pinmode, GPIO17 is referenced as 17
 GPIO.setmode(GPIO.BCM)
 #Setup each channel seperately
-right_channel = Channel(14, 17, 18) #enable, fw, bk
-left_channel = Channel(24, 22, 23)  #enable, fw, bk
+right_channel = Channel(2, 3, 4) #enable, fw, bk
+left_channel = Channel(17, 27, 22)  #enable, fw, bk
 
 #Control vehicle as a whole, or access instance variables to control right or left seperately
 car = Vehicle(right_channel, left_channel)
 
 
 #initialisation of camera
-with picamera.PiCamera() as camera:
-    camera.resolution = (128, 128)
-    with picamera.array.PiRGBArray(camera) as output:
+if True:
+    with picamera.PiCamera() as camera:
         while True:
+            camera.resolution = (128, 128)
+            camera.framerate = 30
+            output = np.empty((128,128,3), dtype=np.uint8)
+            camera.capture(output, 'rgb')
+            acc = 0
+            n = output[:,:,0].size
+            acc = np.sum(output[:,:, 0])
+            # for i in output[0]:
+            #     acc += int(i[0])
+            #     n += 1
+            print(type(acc))
+            redness = acc/n
+
+            print(f'redness = {redness} out of 256')
+            if redness > 130:
+                car.stop()
+            else:
+                car.drive(100, 'fw')
+
+            #sleep(0.2)
+
+
+
+
+
+    """ what callum wrote incase i break it 
+    with picamera.array.PiRGBArray(camera) as output:               
+        while True:
+
             camera.capture(output, 'rgb')
             arr = output.array
             #Define crop
@@ -26,7 +56,9 @@ with picamera.PiCamera() as camera:
             #Count colour function
             #Make decisions
             #Move car
-            sleep(0.1)
+            print(arr)
+            sleep(5)
+        """
 
 
 
@@ -34,15 +66,7 @@ with picamera.PiCamera() as camera:
 #Right channel seems to be able to handle a lower duty cycle than left
 #Increasing the PWM frequency might help with this, however needs more thought
 if False:
-    car.left.drive(60, 'fw')
-    car.right.drive(80, 'bk')
-    sleep(4)
-    car.stop()
-    car.drive(40, 'fw')
-    sleep(3)
-    car.stop()
-    car.drive(70, 'bk')
-    sleep(2)
-    car.drive(90, 'fw')
-    sleep(5)
-    car.stop()
+    car.left.stop()
+    car.right.stop()
+    #car.right.drive(80, 'fw')
+   
