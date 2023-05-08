c712b07c905b4109f5e9516a848b9c19a4d47cdf
David-Helmerson <davidfhelmerso@gmail.com>
2023-05-08 16:44:59 +1000

    Documentation and cleanup

--- mcav_av_workshop/driving_tools.py
+++ mcav_av_workshop/driving_tools.py
@@ -2,14 +2,27 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 
-class Channel():
-    def __init__(self, enable_pin, forward_pin, backward_pin):
-        #Setting up pins
-        GPIO.setup(enable_pin, GPIO.OUT)    #Enable
-        GPIO.setup(forward_pin, GPIO.OUT)    #Foward
-        GPIO.setup(backward_pin, GPIO.OUT)    #Backward
-        #Setting output of enable to software PWM
-        self.pwm = GPIO.PWM(enable_pin, 5000) #PWM on enable pin
+class MotorChannel():
+    """
+    Control class for [MODEL] motor, via RPi GPIO pins
+    
+    Arguments
+    ---------
+    enable_pin : int
+        GPIO pin number for enabling the motor
+    forward_pin : int
+        GPIO pin number for forward driving of the motor
+    backward_pin : int
+        GPIO pin number for forward driving of the motor
+    """
+    def __init__(self, enable_pin: int, forward_pin: int, backward_pin: int):
+        # Setting up pins
+        GPIO.setup(enable_pin, GPIO.OUT)       # Enable
+        GPIO.setup(forward_pin, GPIO.OUT)      # Foward
+        GPIO.setup(backward_pin, GPIO.OUT)     # Backward
+
+        # Setting output of enable to software PWM
+        self.pwm = GPIO.PWM(enable_pin, 5000)  # PWM on enable pin
         self.pwm.start(0)
 
         #GPIO pin numbers
@@ -17,31 +30,63 @@ class Channel():
         self.fw = forward_pin
         self.bk = backward_pin
     
-    def drive(self, duty_cycle:int, direction: str):
-        #Set direction pin high, other low
-        GPIO.output(self.fw, direction == 'fw')
-        GPIO.output(self.bk, direction == 'bk')
+    def drive(self, duty_cycle: int, forward: bool):
+        """
+        Motor driving from duty cycle
 
-        #Change enable to PWM
+        duty_cycle : int
+            Duty cycle for motor driving PWM signal
+        forward : bool
+            Motor driving direction, True for forward and False for reverse
+        """
+        # Set direction pin high, other low
+        GPIO.output(self.fw, forward)
+        GPIO.output(self.bk, not forward)
+
+        # Change enable to PWM
         self.pwm.ChangeDutyCycle(duty_cycle)
 
     def stop(self):
+        """Stops motor operation"""
         self.pwm.ChangeDutyCycle(0)
 
 
 class Vehicle():
-    def __init__(self, right: Channel, left: Channel):
-        self.right = right
-        self.left = left
+    """
+    Control class for differential drive vehicle
+
+    Arguments
+    ---------
+    right_channel : MotorChannel
+        Output specification for right motor
+    left_channel : MotorChannel
+        Output specification for left motor
+    wheel_r : float
+        Radius of driven/virtual wheel
+    baseline : float
+        Horizontal distance between left/right mean ground contacts
+    """
+    def __init__(self, right_channel: MotorChannel, left_channel: MotorChannel, wheel_r: float, baseline: float):
+        self.motor_right = right_channel
+        self.motor_left = left_channel
+        self.wheel_r = wheel_r
+        self.baseline = baseline
     
-    def drive_linang(self, lin, ang):
+    def drive_linang(self, lin: float, ang: float):
+        """
+        Sets the linear and angular velocity of the vehicle
+
+        Arguments
+        ---------
+        lin : float
+            Linear velocity of vehicle in meters per second
+        ang : float
+            Angular velocity of vehicle in radians per second
+        """
         # TODO: Math for driving motors from linear/angular velocity
         pass
 
     def stop(self):
-        self.right.stop()
-        self.left.stop()
-
-    def drive(self, duty_cycle, direction):
-        self.right.drive(duty_cycle, direction)
-        self.left.drive(duty_cycle, direction)
+        """Stops vehicle driving"""
+        self.motor_right.stop()
+        self.motor_left.stop()
--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import sys
 import numpy as np
 from time import sleep
 import RPi.GPIO as GPIO
-from mcav_av_workshop.driving_tools import Vehicle, Channel
+from mcav_av_workshop.driving_tools import Vehicle, MotorChannel
 from behaviour import camera_response
 
 
@@ -18,15 +18,13 @@ if __name__ == '__main__':
     parser.add_argument('--resolution', default=(128, 128), type=tuple, help='Resolution of the vehicle\'s camera')
     parser.add_argument('--framerate', default=10, type=int, help='Frame rate of the vehicle\'s camera (fps)')
     args = parser.parse_args()
-    print(args.resolution)
-    print(args.framerate)
 
     # Pinmode, GPIO17 is referenced as 17
     GPIO.setmode(GPIO.BCM)
-    # Setup each channel seperately
-    right_channel = Channel(2, 3, 4)            # enable, fw, bk
-    left_channel = Channel(17, 27, 22)          # enable, fw, bk
-    car = Vehicle(right_channel, left_channel)  # Control vehicle as a whole, or access instance variables to control right or left seperately
+    # Setup each MotorChannel seperately
+    right_MotorChannel = MotorChannel(2, 3, 4)            # enable, fw, bk
+    left_MotorChannel = MotorChannel(17, 27, 22)          # enable, fw, bk
+    car = Vehicle(right_MotorChannel, left_MotorChannel)  # Control vehicle as a whole, or access instance variables to control right or left seperately
 
     # Set up Pi camera and run main loop
     with picamera.PiCamera(resolution=args.resolution, framerate=args.framerate) as camera:
