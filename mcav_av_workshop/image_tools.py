109278196772c169ec5c84876abf1c29a728dca9
David-Helmerson <davidfhelmerson@gmail.com>
2023-07-03 15:26:53 +1000

    minor reformat

--- mcav_av_workshop/image_tools.py
+++ mcav_av_workshop/image_tools.py
@@ -6,16 +6,19 @@ import numpy as np
 import cv2
 from cv2 import aruco, COLOR_BGR2GRAY
 
-from mcav_av_workshop.config import ARUCO_SIZE, ARUCO_DICT, ARUCO_PARAMS, CAMERA_MATRIX
+from mcav_av_workshop import config
+
+
+
 
 def aruco_pos(image):
     """
     Find position of visible ArUco markers in image, relative to vehicle
     """
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # Change grayscale
-    corners, ids, _ = aruco.detectMarkers(gray_image, ARUCO_DICT, parameters=ARUCO_PARAMS)
+    gray_image = cv2.cvtColor(image, COLOR_BGR2GRAY)  # Change grayscale
+    corners, ids, _ = aruco.detectMarkers(gray_image, config.ARUCO_DICT, parameters=config.ARUCO_PARAMS)
 
-    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, ARUCO_SIZE, CAMERA_MATRIX, None)
+    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, config.ARUCO_SIZE, config.CAMERA_INTRINSIC, config.CAMERA_DISTORTION)
 
     # TODO: return relative position of aruco markers
           
