75412a98065decd786c4264d27ed396410497d08
Josh <j.p.lu@outlook.com>
2023-11-08 17:44:06 +1100

    feat: save images from picam2

--- mcav_av_workshop/camera/camera.py
+++ mcav_av_workshop/camera/camera.py
@@ -1,20 +1,35 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 import time
-import cv2 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Define the Camera Variable 
-picam2 = Picamera2()
-# Initiate the PiCamera
-picam2.start()
-time.sleep(1)
+def capture_full_res(picam: Picamera2, output_file: str):
+  capture_config = picam.create_still_configuration()
+  picam.switch_mode_and_capture_file(capture_config, output_file)
 
-# Request for the capturing of an image and convert it into array. 
-array = picam2.capture_array("main")
-print(array.shape)
 
-#rgba = cv2.cvtColor(array, cv2.COLOR_RGB2RGBA)
-plt.imshow(array)
-plt.show()
+if __name__ == '__main__':
+  # Define the Camera Variable
+  picam2 = Picamera2()
 
+  # Preview
+  picam2.start_preview(Preview.QTGL)
+  preview_config = picam2.create_preview_configuration()
+  picam2.configure(preview_config)
+
+  # Initiate the PiCamera
+  picam2.start()
+  time.sleep(2)
+
+  capture_full_res(picam=picam2, output_file="output_3.jpg")
+
+  time.sleep(5)
+
+  # # Request for the capturing of an image and convert it into array.
+  # array = picam2.capture_array("main")
+  # print(array.shape)
+
+  # #rgba = cv2.cvtColor(array, cv2.COLOR_RGB2RGBA)
+  # plt.imshow(array)
+  # plt.show()
