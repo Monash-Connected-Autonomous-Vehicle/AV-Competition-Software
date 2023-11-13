f74fe86dae1609b7c1dce8e3bf6af7002de756ec
Josh <j.p.lu@outlook.com>
2023-11-14 00:28:14 +1100

    feat: run roboflow model

--- mcav_av_workshop/camera/camera.py
+++ mcav_av_workshop/camera/camera.py
@@ -1,31 +1,49 @@
 from picamera2 import Picamera2, Preview
+from dotenv import load_dotenv
+from roboflow import Roboflow
+import os
 import time
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
+import datetime
+
+load_dotenv()
 
 
-def capture_full_res(picam: Picamera2, output_file: str):
-    capture_config = picam.create_still_configuration()
-    picam.switch_mode_and_capture_file(capture_config, output_file)
+def run_model(image_file: float):
+    """
+    Runs trained Roboflow model hosted locally
+    :param image_file: image file name without extension
+    """
+    rf = Roboflow(api_key='{}'.format(os.environ.get('ROBOFLOW_KEY')))
+    project = rf.workspace().project('codedrive-traffic-lights')
+    model = project.version(1).model
+    prediction = model.predict('data/{}.jpg'.format(image_file), confidence=50, overlap=50).json()
+    print(prediction)
+    model.predict('data/{}.jpg'.format(image_file), confidence=50, overlap=50).save(
+        'output/{}-model.jpg'.format(image_file))
 
 
 if __name__ == '__main__':
     # Define the Camera Variable
     picam2 = Picamera2()
 
-    # Preview
-    picam2.start_preview(Preview.QTGL)
-    preview_config = picam2.create_preview_configuration()
-    picam2.configure(preview_config)
+    # Initialise config
+    preview_config = picam2.create_preview_configuration(main={"size": (1640, 1232)})
+    # TODO: Look into resizing the captured image
+    capture_config = picam2.create_still_configuration(main={"size": (1640, 1232)})
+    # capture_config = picam2.create_still_configuration(main={ "size": (820, 616) })
 
     # Initiate the PiCamera
+    picam2.configure(preview_config)
+    picam2.start_preview(Preview.QTGL)
     picam2.start()
-    time.sleep(2)
 
-    capture_full_res(picam=picam2, output_file="output_3.jpg")
+    while True:
+        timestamp = datetime.datetime.now()
+        unix_timestamp = time.mktime(timestamp.timetuple())
+        time.sleep(5)
 
-    time.sleep(5)
+        picam2.switch_mode_and_capture_file(capture_config, file_output="data/{}.jpg".format(unix_timestamp))
+        run_model(unix_timestamp)
 
     # # Request for the capturing of an image and convert it into array.
     # array = picam2.capture_array("main")
